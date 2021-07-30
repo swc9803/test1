3aa75b762b2337669d461d4512daa68ae5a502df
swc9803 <swc9803@gmail.com>
2021-07-30 20:27:21 +0900

    0730

--- src/pages/Test9.vue
+++ src/pages/Test9.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="wrapper">
     <div class="scrollElement" />
-    <Scroll class="Scroll svg" />
+    <Scroll class="svg" />
     <Fall class="svg" />
-    <!-- <Leaf1 class="Leaf1 Leaf svg" />
-    <Leaf2 class="Leaf2 Leaf svg" />
-    <Leaf3 class="Leaf3 Leaf svg" /> -->
+    <Leaf/>
   </div>
 </template>
 
 <script>
-import Fall from '@/components/fall/Fall'
-// import Leaf1 from '@/components/fall/Leaf1'
-// import Leaf2 from '@/components/fall/Leaf2'
-// import Leaf3 from '@/components/fall/Leaf3'
 import Scroll from '@/components/fall/Scroll'
+import Fall from '@/components/fall/Fall'
+import Leaf from '@/components/fall/Leaf'
 
 import { onMounted } from 'vue'
 import { gsap } from 'gsap'
@@ -24,25 +20,15 @@ gsap.registerPlugin(ScrollTrigger)
 export default {
   components: {
     Scroll,
-    Fall
-    // Leaf1, Leaf2, Leaf3,
+    Fall,
+    Leaf
   },
   setup () {
     window.onbeforeunload = function () {
       window.scrollTo(0, 0)
     }
     onMounted(() => {
-      const Scroll = gsap.timeline()
-      ScrollTrigger.create({
-        animation: Scroll,
-        trigger: '.scrollElement',
-        start: 'top top',
-        end: '4% top',
-        scrub: 2,
-        markers: true
-      })
-      Scroll.to('.Scroll', { opacity: 0 }, 0)
-      Scroll.to('.Scroll', { y: 100 }, 0)
+
     })
   }
 }
@@ -64,13 +50,9 @@ export default {
 }
 .scrollElement {
   position: absolute;
-  height: 6000px;
+  height: 8000px;
   width: 100px;
   top: 0;
   opacity: 0;
-}
-.rotate1{
-  position: fixed;
-  display: block;
 }
 </style>
